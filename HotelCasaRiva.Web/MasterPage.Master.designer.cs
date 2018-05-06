a36b7946eba8919bdfb6b69613dadd5b0199e181
Erik Khanpara <erik.khanpara@gmail.com>
2018-05-06 16:14:22 +0530

    loginform updated

--- HotelCasaRiva.Web/MasterPage.Master.designer.cs
+++ HotelCasaRiva.Web/MasterPage.Master.designer.cs
@@ -76,6 +76,24 @@ namespace HotelCasaRiva.Web {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
         
         /// <summary>
+        /// ScriptManager1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.ScriptManager ScriptManager1;
+        
+        /// <summary>
+        /// UpdatePanel1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.UpdatePanel UpdatePanel1;
+        
+        /// <summary>
         /// Email control.
         /// </summary>
         /// <remarks>
